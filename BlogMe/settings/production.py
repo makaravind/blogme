921bdba1f712e383647a2880ca5919af150561f1
makaravind <makaravind11195@gmail.com>
2016-07-10 12:31:07 +0530

    add requirments

---
+++ BlogMe/settings/production.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+import os
+DEGUG = False
+TEMPLATE_DEBUG = True
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+settings.DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
